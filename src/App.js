cd68e4460d3b45eee6ddf4cfd7e418087e4e7a31
reddynie11 <reddy.nie11@gmail.com>
2019-08-27 23:21:06 +0530

    Nesting components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Me from './Me';
 
 class App extends React.Component{
   state ={
@@ -16,6 +17,7 @@ class App extends React.Component{
   render(){
     return(
       <div>
+        <Me />
         {this.state.name}
         <h2>Please enter your name below</h2>
         <input type='text' onChange={this.handleChange}/>
---
+++ src/Me.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { directive } from '@babel/types';
+import App from './App';
+
+class Me extends React.Component{
+    render(){
+        return(
+            <div>
+                <ul>
+                    <li>Narasimha Reddy</li>
+                    <li>An upcoming MERN stack Deeveloper</li>
+                </ul>
+            </div>
+        )
+    }
+}
+export default Me;
