ebfad44a8c6893704b1c1d052fc14a1197999a20
reddynie11 <reddy.nie11@gmail.com>
2019-08-27 23:11:08 +0530

    events

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state ={
+    name : ''
+  }
+  handleChange=(e)=>{
+    this.setState({
+      name: e.target.value
+    })
+  }
+  handleClick =(e)=>{
+    e.preventDefault();
+    }
+  
+  render(){
+    return(
+      <div>
+        {this.state.name}
+        <h2>Please enter your name below</h2>
+        <input type='text' onChange={this.handleChange}/>
+        <button onClick={this.handleClick}  >Submit</button>
+
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
