8a40d6d312b6fab79f1b5da81962563267a14dab
reddynie11 <reddy.nie11@gmail.com>
2019-08-28 21:10:39 +0530

    conditional output- if else

--- src/Friends.js
+++ src/Friends.js
@@ -1,44 +1,46 @@
 import React from 'react';
 
-
-
-// const Friends = ({data})=>{
-//    return(
-//     <div>
-//     {  data.map((frnd)=>{
-//           return(
-//               <div key={frnd.id} style={{'border' : '1px solid black'}}>
-//                   <div>{frnd.name}</div>
-//                   <div>{frnd.age}</div>
-//                   <div>{frnd.city}</div>
-//               </div>
-//           )
-//       })}
-//   </div>
-//    )
-// }
-
-class Friends extends React.Component{
-    state={
-
-    }
-    render(){
-        let data=this.props.data;
+const Friends = ({data})=>{
     
         return(
             <div>
-              {  data.map((frnd)=>{
-                    return(
-                        <div key={frnd.id} style={{'border' : '1px solid black'}}>
-                            <div>{frnd.name}</div>
-                            <div>{frnd.age}</div>
-                            <div>{frnd.city}</div>
-                        </div>
-                    )
+                {data.map((frnd)=>{
+                    if (frnd.age<34){
+                        return(
+                            <div key={frnd.id} style={{'border' : '1px solid black'}}>
+                                <div>{frnd.name}</div>
+                                <div>{frnd.age}</div>
+                                <div>{frnd.city}</div>
+                            </div>
+                        )
+                    }else return null;
                 })}
             </div>
         )
-    }
+    
 }
-
 export default Friends
+
+// class Friends extends React.Component{
+//     state={
+
+//     }
+//     render(){
+//         let data=this.props.data;
+    
+//         return(
+//             <div>
+//               {  data.map((frnd)=>{
+//                     return(
+//                         <div key={frnd.id} style={{'border' : '1px solid black'}}>
+//                             <div>{frnd.name}</div>
+//                             <div>{frnd.age}</div>
+//                             <div>{frnd.city}</div>
+//                         </div>
+//                     )
+//                 })}
+//             </div>
+//         )
+//     }
+// }
+
