767a6523db43f6d0f3fab796b379dbbe74ea389f
reddynie11 <reddy.nie11@gmail.com>
2019-08-28 20:49:42 +0530

    class component

--- src/Friends.js
+++ src/Friends.js
@@ -2,42 +2,43 @@ import React from 'react';
 
 
 
-const Friends = ({data})=>{
-   return(
-    <div>
-    {  data.map((frnd)=>{
-          return(
-              <div key={frnd.id} style={{'border' : '1px solid black'}}>
-                  <div>{frnd.name}</div>
-                  <div>{frnd.age}</div>
-                  <div>{frnd.city}</div>
-              </div>
-          )
-      })}
-  </div>
-   )
+// const Friends = ({data})=>{
+//    return(
+//     <div>
+//     {  data.map((frnd)=>{
+//           return(
+//               <div key={frnd.id} style={{'border' : '1px solid black'}}>
+//                   <div>{frnd.name}</div>
+//                   <div>{frnd.age}</div>
+//                   <div>{frnd.city}</div>
+//               </div>
+//           )
+//       })}
+//   </div>
+//    )
+// }
+
+class Friends extends React.Component{
+    state={
+
+    }
+    render(){
+        let data=this.props.data;
+    
+        return(
+            <div>
+              {  data.map((frnd)=>{
+                    return(
+                        <div key={frnd.id} style={{'border' : '1px solid black'}}>
+                            <div>{frnd.name}</div>
+                            <div>{frnd.age}</div>
+                            <div>{frnd.city}</div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
-// class Friends extends React.Component{
-//     state={
-
-//     }
-//     render(){
-//         let data=this.props.data;
-    
-//         return(
-//             <div>
-//               {  data.map((frnd)=>{
-//                     return(
-//                         <div key={frnd.id} style={{'border' : '1px solid black'}}>
-//                             <div>{frnd.name}</div>
-//                             <div>{frnd.age}</div>
-//                             <div>{frnd.city}</div>
-//                         </div>
-//                     )
-//                 })}
-//             </div>
-//         )
-//     }
-// }
 export default Friends
