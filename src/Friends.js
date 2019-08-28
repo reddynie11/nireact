35a6f6a35b5bbe23deaea7819f87da65f238090f
reddynie11 <reddy.nie11@gmail.com>
2019-08-28 22:58:41 +0530

    styling

--- src/Friends.js
+++ src/Friends.js
@@ -4,17 +4,20 @@ const Friends = ({data})=>{
     
         return(
             <div>
-                {data.map((frnd)=>{
-                    
-                        return frnd.age < 34 ? (
-                            <div key={frnd.id} style={{'border' : '1px solid black'}}>
-                                <div>{frnd.name}</div>
-                                <div>{frnd.age}</div>
-                                <div>{frnd.city}</div>
+                <h2>Friends</h2>
+                <div style={{'display':'flex','flexDirection':'row'}} >
+                    {data.map((frnd)=>{
+                        return(
+                            <div key={frnd.id} style={{'border' : '1px solid black', 'width':'200px','padding':'10px','margin':'5px','flex-grow':'3','textAlign':'center'}}>
+                                    <div>Name: {frnd.name}</div>
+                                    <div>Age: {frnd.age}</div>
+                                    <div>City: {frnd.city}</div>
                             </div>
-                        ):null
-                    
-                })}
+                            
+                        )
+                        
+                    })}
+                </div>
             </div>
         )
     
